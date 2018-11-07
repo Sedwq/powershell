a7c5196fc86abec33d18008f1cbb088bb30a94db
Sedwq <ced.demar@gmail.com>
2018-11-07 15:06:08 +0100

    Update pwersql.ps1

--- pwersql.ps1
+++ pwersql.ps1
@@ -1,7 +1,8 @@
 #Version du 12 juillet 2018
 
 Clear-Host
-$MonTableau = @('bdd1','bdd2','bdd3')
+#$MonTableau = @('bdd1','bdd2','bdd3')
+$MonTableau = Get-Content liste_bdd.txt
 
 
 #Méthode 1
@@ -9,7 +10,7 @@ foreach ($item in $MonTableau){
 	 $pwd = 'xxxxxxxxx'
 	 $command = "system/$pwd@$($item)"
 	 #Write-Host "sqlplus  -L $command 'C:\Master\@reque.sql'"
-	 sqlplus -s -L $command "@reque.sql" | out-file -Append 'c:\master\sql-output.txt'
+	 sqlplus -s -L $command "@reque.sql" | Select -Index 3,11 | out-file -Append 'c:\master\sql-output.txt'
 }
 
 # reste à gérer les erreurs de connexion, le formatage du fichier de sortie etc
