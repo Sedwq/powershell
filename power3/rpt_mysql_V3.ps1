99fde088f6c99e37a3fe19dbb521fbd8d9b0c41d
Sedwq <ced.demar@gmail.com>
2019-01-23 16:17:01 +0100

    Update rpt_mysql_V3.ps1

--- power3/rpt_mysql_V3.ps1
+++ power3/rpt_mysql_V3.ps1
@@ -1,11 +1,13 @@
 ############################################################################################
 #
 #  Rapport mail MYSQL
-#  CD V3.0 du 21/01/2019 Amélioration perf du script
+#  CD V3.0 du 23/01/2019 Amélioration perf du script
 #
 ############################################################################################
 #Variables
-$MonTableau = Get-Content C:\path\fic_list1.txt
+$MonTableau = Get-Content C:\path\liste_bdd_prd.txt
+
+#$MonTableau = Get-Content C:\exploit\liste_bdd_prd.txt
 $objHashTableprd = @{} #Initialisation d'une hash table
 $objHashTableprd2 = @{} #Initialisation d'une hash table
 $objHashTablerct = @{} #Initialisation d'une hash table
@@ -13,14 +15,13 @@ $objHashTablerct2 = @{} #Initialisation d'une hash table
 $Time = Get-Date
 $fichtml="C:\path\fictosend.html"
 
-$From = "XXXXXXX@XXXXXXX"
-$To = "xxxxxxxx@xxxxxxxx.xx"
-$SMTPServer = "XXXXX"
-#$Username = "toto"
+$From = "XX@XX"
+$To = "XX@XX"
+$SMTPServer = "XXXX"
 $subject = "Rapport des bases MySQL OK"
 
-$user = "xxxxxxxxxx"
-$pwd = 'XXXXXXXX'
+$user = "XXXXX"
+$pwd = 'XXXXXXXXX'
 $MYSQL = "C:\Program Files\MySQL\MySQL Server 5.0\bin\mysql.exe"
 $query = "\s"
 
@@ -58,11 +59,11 @@ $htm | out-file $fichtml
 $i=0    
 $objHashTableprd.GetEnumerator() | ForEach-Object{
     $i=++$i
-    $htm2 = '<TD BGCOLOR={1}><a href="#{0}">{0}</a></TD>' -f $_.key, $_.value
-    $htm2 | out-file $fichtml -append
+    $htm = '<TD BGCOLOR={1}><a href="#{0}">{0}</a></TD>' -f $_.key, $_.value
+    $htm | out-file $fichtml -append
 	if($i % 5 -eq 0){
-	$htm3 = '</TR>'
-	$htm3 | out-file $fichtml -append
+	$htm = '</TR>'
+	$htm | out-file $fichtml -append
 	}
 	
 }
@@ -70,17 +71,7 @@ $objHashTableprd.GetEnumerator() | ForEach-Object{
 $htm1 = '</TABLE></BR></BR></BR>'
 $htm1 | out-file $fichtml -append
     
-#Suppression du/des serveur(s) en echec de connexion
-$ht = $objHashTableprd.Clone()
-    
-foreach($k in $objHashTableprd.GetEnumerator()){ 
-    if($k.Value -eq "RED"){
-        #notice, deleting from clone, then return clone at the end
-        $ht.Remove($k.Key) 
-        }
-}
-
-# Construction corps fichier HTML recette
+# Construction corps fichier HTML production
 
 $htm = '</BR><TABLE border=1 cellspacing=0 cellpadding=10><TD bgcolor=#CCCCCC width=360><a id=production></a>mysql - <i>production</i></TD>
             <TD bgcolor=#CCCCCC width=180> Version MySQL </TD>
@@ -111,7 +102,7 @@ $htm2 | out-file $fichtml -append
 ############################################################################################
 #recette
 ############################################################################################
-$MonTableau2 = Get-Content C:\path\fic_list2.txt
+$MonTableau2 = Get-Content C:\path\liste_bdd_rec.txt
 
 # Tests de connexion au serveur Mysql
 foreach ($item in $MonTableau2){
@@ -142,27 +133,17 @@ $htm | out-file $fichtml -append
 $j=0
 $objHashTablerct.GetEnumerator() | ForEach-Object{
     $j=++$j
-    $htm9 = '<TD BGCOLOR={1}><a href="#{0}">{0}</a></TD>' -f $_.key, $_.value
-    $htm9 | out-file $fichtml -append
+    $htm = '<TD BGCOLOR={1}><a href="#{0}">{0}</a></TD>' -f $_.key, $_.value
+    $htm | out-file $fichtml -append
 	if($j % 5 -eq 0){
-	$htm10 = '</TR>'
-	$htm10 | out-file $fichtml -append
+	$htm = '</TR>'
+	$htm | out-file $fichtml -append
 	}
 }
 	
 $htm1 | out-file $fichtml -append
-    
-# Suppression du/des serveur(s) en echec de connexion
-$ht = $objHashTablerct.Clone()
-    
-foreach($k in $objHashTablerct.GetEnumerator()){ 
-    if($k.Value -eq "RED"){
-        #notice, deleting from clone, then return clone at the end
-        $ht.Remove($k.Key) 
-        }
-}
 
-# Construction corps fichier HTML rectte
+# Construction du corps fichier HTML recette
 
 $htm = '</BR><TABLE border=1 cellspacing=0 cellpadding=10><TD bgcolor=#CCCCCC width=360><a id=recette></a>mysql - <i>recette</i></TD>
             <TD bgcolor=#CCCCCC width=180> Version MySQL </TD>
